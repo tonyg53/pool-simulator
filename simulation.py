d01a467dbf3815701f0dd9e117d26af99cecaea6
tony.gold <tony.gold@prismdefence.com>
2018-07-13 17:29:50 +0930

    still adding collisions

--- simulation.py
+++ simulation.py
@@ -5,7 +5,7 @@ Created on Thu Jul 12 15:53:17 2018
 @author: tony.gold
 """
 import tableballdefs
-import collide
+import collision
 import math
 import itertools
 
@@ -17,10 +17,13 @@ def main():
     for i in range(0,numBalls):
         ballList.append( tableballdefs.Ball() )
         
-    ballList[0].xLoc = table.length / 4
-    ballList[0].yLoc = table.width / 2
-    ballList[1].xLoc = table.length * 3 / 4
-    ballList[1].yLoc = table.width / 2
+    ballList[0].Loc.x = table.length / 4
+    ballList[0].Loc.y = table.width / 2
+    ballList[1].Loc.x = table.length * 3 / 4
+    ballList[1].Loc.y = table.width / 2
+    
+    for i, ball in enumerate(ballList):
+        print("ball",i, ball.Vel.x, ball.Vel.y, ball.diameter, ball.mass, ball.elasticity, ball.Loc.x, ball.Loc.y, ball.english, ball.topSpin)
     
     brake = tableballdefs.Shot(ballList[0])
     brake.execute()
@@ -29,32 +32,36 @@ def main():
     stopVel = 0.05
     elapsedTime = 0
     
+    for i, ball in enumerate(ballList):
+        print("ball",i, ball.Vel.x, ball.Vel.y, ball.diameter, ball.mass, ball.elasticity, ball.Loc.x, ball.Loc.y, ball.english, ball.topSpin)
+    
     while ballsMoving :
         countStoppedBalls = 0
         for ball in ballList: 
-            if ball.xVel <= stopVel and ball.xVel >= -stopVel and ball.yVel <= stopVel and ball.yVel >= -stopVel: 
-                ball.xVel = 0
-                ball.yVel = 0
+            
+            if ball.Vel.x <= stopVel and ball.Vel.x >= -stopVel: 
+                ball.zeroVel()
                 countStoppedBalls = countStoppedBalls + 1
+                
                     
             else:
                 ballsMoving = True
                 #check to see if the ball has hit a wall, if so reverse the velocity dir
-                if ball.xLoc >= table.length or ball.xLoc <= 0 : 
-                    ball.xVel = -1 * table.cushionBounce * ball.xVel
-                if ball.yLoc >= table.width or ball.yLoc <= 0 : 
-                    ball.yVel = -1 * table.cushionBounce * ball.yVel
+                if ball.Loc.x >= table.length or ball.Loc.x <= 0 : 
+                    ball.Vel.x = -1 * table.cushionBounce * ball.Vel.x
+                if ball.Loc.y >= table.width or ball.Loc.y <= 0 : 
+                    ball.Vel.y = -1 * table.cushionBounce * ball.Vel.y
                 
                 #update the ball velocity from the acceleration
-                ball.xLoc = ball.xLoc + (ball.xVel * timeStep)
-                ball.yLoc = ball.yLoc + (ball.yVel * timeStep)
+                ball.Loc.x = ball.Loc.x + (ball.Vel.x * timeStep)
+                ball.Loc.y = ball.Loc.y + (ball.Vel.y * timeStep)
                 radius = ball.diameter / 2
                 
                 #calculate the acceleration based on the table and ball conditions
                 acc = -(2*9.8*table.feltThickness)/(3*radius*radius)
-                if ball.xVel >= 0 : xPositive = True
+                if ball.Vel.x >= 0 : xPositive = True
                 else: xPositive = False
-                alpha = math.atan(ball.yVel/ball.xVel)
+                alpha = math.atan(ball.Vel.y/ball.Vel.x)
                 
                 #this code accounts for ball moving in negitave directions
                 if xPositive:
@@ -63,13 +70,13 @@ def main():
                     xAcc = -1 * math.cos(alpha) * acc
                 yAcc = math.sin(alpha) * acc
                 
-                ball.xVel = ball.xVel + (xAcc * timeStep)
-                ball.yVel = ball.yVel + (yAcc * timeStep)
+                ball.Vel.x = ball.Vel.x + (xAcc * timeStep)
+                ball.Vel.y = ball.Vel.y + (yAcc * timeStep)
                 
-                print("time :", elapsedTime, "x loc", ball.xLoc, "y loc: ",ball.yLoc, " x vel: ",ball.xVel," x acc: ",xAcc)
+                print("time:", round(elapsedTime,2), "  x loc:", round(ball.Loc.x,2), "  y loc:",round(ball.Loc.y,2), "  x vel:",round(ball.Vel.x,2),"  x acc:",round(xAcc,2))
                 #input()
-#        for ball1, ball2 in itertools.combinations(ballList, 2):
-#             collide.run(ball1, ball2)
+        for ball1, ball2 in itertools.combinations(ballList, 2):
+             if collision.happened(ball1, ball2) : collision.run(ball1, ball2)
                 
         if countStoppedBalls == numBalls : ballsMoving = False
         elapsedTime = elapsedTime + timeStep
--- tableballdefs.py
+++ tableballdefs.py
@@ -6,9 +6,18 @@ Created on Mon Jul  9 16:35:15 2018
 """
 import math
 
+class Vector(object):
+    
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        
+    def getLength(self):
+        return math.hypot(self.x,self.y)
+
 class Table(object):
     
-    def __init__(self, length = 9, feltThickness = 0.000005, feltFrictionCo = 0.1, cushionBounce = 0.65):
+    def __init__(self, length = 9, feltThickness = 0.00005, feltFrictionCo = 0.1, cushionBounce = 0.9):
         self.length = length * 0.3048
         self.width = (length / 2) * 0.3048
         self.feltThickness = feltThickness
@@ -17,20 +26,21 @@ class Table(object):
 
 class Ball(object):
     
-    def __init__(self, xVel = 0, yVel=0, diameter = 0.0572, mass = 0.165, elasticity = 0.99, xLoc = 0, yLoc = 0, english = 0, topSpin = 0):
+    def __init__(self, xVel = 0, yVel = 0, diameter = 0.0572, mass = 0.165, elasticity = 0.99, xLoc = 0, yLoc = 0, english = 0, topSpin = 0):
         self.diameter = diameter
         self.mass = mass
         self.elasticity = elasticity
-        self.xVel = xVel
-        self.yVel = yVel
-        self.xLoc = xLoc
-        self.yLoc = yLoc
+        self.Vel = Vector(xVel,yVel)
+        self.Loc = Vector(xLoc,yLoc)
         self.english = english
         self.topSpin = topSpin
         
+    def zeroVel(self):
+        self.Vel = Vector(0,0)
+        
 class Shot(object):
     
-    def __init__(self, cueBall, cueStickVelocity = 2, shotAzmuth = 0, cueStickMass = 20, cueStickCOR = 0.85, strikePtDistFromCenter = 0, strikePtAngle = 0):
+    def __init__(self, cueBall, cueStickVelocity = 2, shotAzmuth = 0, cueStickMass = 20, cueStickCOR = 0.98, strikePtDistFromCenter = 0, strikePtAngle = 0):
         self.cueStickMass = cueStickMass * 0.0283495
         self.cueStickVelocity = cueStickVelocity
         self.cueStickCOR = cueStickCOR
@@ -40,17 +50,14 @@ class Shot(object):
         self.cueBall = cueBall
         
     def execute(self):
-        if self.cueBall.xVel != 0 and self.cueBall.yVel !=0 : raise "wait for balls to stop"
+        if self.cueBall.Vel.length != 0 : raise "wait for balls to stop"
 
         stickForce = self.cueStickMass * self.cueStickVelocity
         ballVel = stickForce/(self.cueBall.mass + self.cueStickMass)
-        self.cueBall.xVel = math.cos(math.radians(self.shotAzmuth))*ballVel
-        self.cueBall.yVel = math.sin(math.radians(self.shotAzmuth))*ballVel
+        self.cueBall.Vel.x = math.cos(math.radians(self.shotAzmuth))*ballVel
+        self.cueBall.Vel.y = math.sin(math.radians(self.shotAzmuth))*ballVel
         return self.cueBall
 
-def zeroVels(ball):
-    ball.xVel = 0
-    ball.yVel = 0
-    return ball
+
       
     
